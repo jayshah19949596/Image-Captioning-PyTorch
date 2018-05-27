313b8d9c6a8b42ae0dc6b3f3656f0ccfa4e72bf5
Jai Shah <jai.s@somaiya.edu>
2018-05-27 18:11:59 -0500

    first commit

--- model.py
+++ model.py
@@ -7,16 +7,16 @@ from torch.nn.utils.rnn import pack_padded_sequence
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
         super(EncoderCNN, self).__init__()
-        resnet = models.resnet50(pretrained=True)
-        for param in resnet.parameters():
+        res_net = models.resnet50(pretrained=True)
+        for param in res_net.parameters():
             param.requires_grad_(False)
         
-        modules = list(resnet.children())[:-1]
-        self.resnet = nn.Sequential(*modules)
-        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        modules = list(res_net.children())[:-1]
+        self.res_net = nn.Sequential(*modules)
+        self.embed = nn.Linear(res_net.fc.in_features, embed_size)
 
     def forward(self, images):
-        features = self.resnet(images)
+        features = self.res_net(images)
         features = features.view(features.size(0), -1)
         features = self.embed(features)
         return features
@@ -30,8 +30,7 @@ class DecoderRNN(nn.Module):
         self.embed = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_size, vocab_size)
-        
-        
+
     def forward(self, features, captions):
         """Decode image feature vectors and generates captions."""
         embeddings = self.embed(captions)
@@ -41,10 +40,10 @@ class DecoderRNN(nn.Module):
         return outputs
     
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        """ accepts pre-processed image tensor (inputs) and
+            returns predicted sentence (list of tensor ids of length max_len)
+        """
         sampled_ids = []
-        print(inputs.size())
-
         for i in range(20):
             hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
             outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
